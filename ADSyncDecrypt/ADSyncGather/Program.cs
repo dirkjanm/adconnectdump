5fc9de8627f3f7b07ed5243a4bc1270be483d1dc
Dirk-jan Mollema <dirkjan@outsidersecurity.nl>
2022-12-29 21:14:59 +0100

    updated db locations in c# scripts

--- ADSyncDecrypt/ADSyncGather/Program.cs
+++ ADSyncDecrypt/ADSyncGather/Program.cs
@@ -12,7 +12,9 @@ namespace ADSyncGather
     {
         static void Main(string[] args)
         {
-            using (SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\.\\ADSync;Initial Catalog=ADSync;Connect Timeout=30"))
+            string connectionString = (args.Length == 0) ? "Data Source=(LocalDB)\\.\\ADSync2019;Initial Catalog=ADSync;Connect Timeout=30" : args[0];
+            Console.WriteLine("Opening database {0}", connectionString);
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
                 SqlCommand command = new SqlCommand("SELECT private_configuration_xml, encrypted_configuration FROM mms_management_agent;", conn);
